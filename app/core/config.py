8b87c26aa97c2d6f1e6efe49bb643428c6daa5c7
anurag351 <30868625+anurag351@users.noreply.github.com>
2026-02-07 04:51:17 +0530

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "SparkPro Fire Controls API"
 
     # Correct: type annotation required
-    DATABASE_URL: str = "postgresql+asyncpg://sparkpro_nxyp_user:TJWp8Izffurd2s0z0C9RGFd0yQnPj05l@dpg-d6370nu8alac739spucg-a.render.com:5432/sparkpro_nxyp"
+    DATABASE_URL: str = "postgresql+asyncpg://sparkpro_nxyp_user:TJWp8Izffurd2s0z0C9RGFd0yQnPj05l@dpg-d6370nu8alac739spucg-a.singapore-postgres.render.com/sparkpro_nxyp"
 
     FRONTEND_BUILD_DIR: str = "frontend/build"
     TEMP_PW_EXPIRES_MINUTES: int = 60
